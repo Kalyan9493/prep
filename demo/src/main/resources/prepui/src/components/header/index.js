1ef640fd14298afa569a44b3a6c878b66b378b43
Kalyan9493 <kalyankumarreddykavalakuntla@gmail.com>
2023-01-11 12:59:00 +0530

    UI added

---
+++ demo/src/main/resources/prepui/src/components/header/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './navbarElements'
+import logo from '../../assets/logo.svg'
+
+const Header = () => {
+  return (
+      <Nav>
+        <NavLink to="/">
+          {/* <img src={logo} height='100px' width='100px'></img> */}
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+           <h3>About</h3> 
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            <h3>Services</h3>
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+          <h3>Contact Us</h3> 
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+          <h3>Sign Up</h3> 
+          </NavLink>
+        </NavMenu>
+      </Nav>
+  )
+}
+
+export default Header
